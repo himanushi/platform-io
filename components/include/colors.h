e836c6bf04b7d818dd544303346052a868fb0aa2
himanushi <himanushi@users.noreply.github.com>
2024-02-18 20:35:55 +0900

    add colors

---
+++ components/include/colors.h
@@ -0,0 +1,28 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+// Slate
+// ![COLOR_SLATE_50](https://github.com/himanushi/platform-io/assets/27812830/0d19e56e-7d09-417d-a1e2-9bd6a49e71f4)
+static constexpr int COLOR_SLATE_50 = 0xFFFF;
+// ![COLOR_SLATE_100](https://github.com/himanushi/platform-io/assets/27812830/cba9794f-c22a-4698-a8aa-9e070d8276d4)
+static constexpr int COLOR_SLATE_100 = 0xF7DF;
+// ![COLOR_SLATE_200](https://github.com/himanushi/platform-io/assets/27812830/b9e55c2e-d9e2-412f-8bc4-b504678fc3cc)
+static constexpr int COLOR_SLATE_200 = 0xE73C;
+// ![COLOR_SLATE_300](https://github.com/himanushi/platform-io/assets/27812830/f1723528-9a91-4f01-a4c7-8330c2a652f0)
+static constexpr int COLOR_SLATE_300 = 0xCE59;
+// ![COLOR_SLATE_400](https://github.com/himanushi/platform-io/assets/27812830/d4475865-22cc-45f1-a2c8-497c48a6b234)
+static constexpr int COLOR_SLATE_400 = 0x953B;
+// ![COLOR_SLATE_500](https://github.com/himanushi/platform-io/assets/27812830/715eb2b0-9779-48c9-9b6e-c439f452afe1)
+static constexpr int COLOR_SLATE_500 = 0x63EF;
+// ![COLOR_SLATE_600](https://github.com/himanushi/platform-io/assets/27812830/02cbedd6-9665-426d-893a-c46b7b30e0c4)
+static constexpr int COLOR_SLATE_600 = 0x42B5;
+// ![COLOR_SLATE_700](https://github.com/himanushi/platform-io/assets/27812830/fd485ca5-59e5-4e9c-828f-2a8a8550450a)
+static constexpr int COLOR_SLATE_700 = 0x31CE;
+// ![COLOR_SLATE_800](https://github.com/himanushi/platform-io/assets/27812830/54979b69-608d-4218-ad2f-eafa29537bbc)
+static constexpr int COLOR_SLATE_800 = 0x1945;
+// ![COLOR_SLATE_900](https://github.com/himanushi/platform-io/assets/27812830/91914e7a-63ff-47fb-9755-8e4e5b880fad)
+static constexpr int COLOR_SLATE_900 = 0x08A9;
+// ![COLOR_SLATE_950](https://github.com/himanushi/platform-io/assets/27812830/129fff2e-c897-4554-b2b8-73af4217b765)
+static constexpr int COLOR_SLATE_950 = 0x0022;
+
+#endif
