0b2f45d63ebe160132b0d70e9d1ee77086ec5e33
himanushi <himanushi@users.noreply.github.com>
2024-02-18 20:50:59 +0900

    fix

--- components/include/colors.h
+++ components/include/colors.h
@@ -2,51 +2,51 @@
 #define COLORS_H
 
 // Slate
-// ![COLOR_SLATE_50](https://github.com/himanushi/platform-io/assets/27812830/0d19e56e-7d09-417d-a1e2-9bd6a49e71f4)
+// ![COLOR_SLATE](https://github.com/himanushi/platform-io/assets/27812830/0d19e56e-7d09-417d-a1e2-9bd6a49e71f4)
 static constexpr int COLOR_SLATE_50 = 0xFFFF;
-// ![COLOR_SLATE_100](https://github.com/himanushi/platform-io/assets/27812830/cba9794f-c22a-4698-a8aa-9e070d8276d4)
+// ![COLOR_SLATE](https://github.com/himanushi/platform-io/assets/27812830/cba9794f-c22a-4698-a8aa-9e070d8276d4)
 static constexpr int COLOR_SLATE_100 = 0xF7DF;
-// ![COLOR_SLATE_200](https://github.com/himanushi/platform-io/assets/27812830/b9e55c2e-d9e2-412f-8bc4-b504678fc3cc)
+// ![COLOR_SLATE](https://github.com/himanushi/platform-io/assets/27812830/b9e55c2e-d9e2-412f-8bc4-b504678fc3cc)
 static constexpr int COLOR_SLATE_200 = 0xE73C;
-// ![COLOR_SLATE_300](https://github.com/himanushi/platform-io/assets/27812830/f1723528-9a91-4f01-a4c7-8330c2a652f0)
+// ![COLOR_SLATE](https://github.com/himanushi/platform-io/assets/27812830/f1723528-9a91-4f01-a4c7-8330c2a652f0)
 static constexpr int COLOR_SLATE_300 = 0xCE59;
-// ![COLOR_SLATE_400](https://github.com/himanushi/platform-io/assets/27812830/d4475865-22cc-45f1-a2c8-497c48a6b234)
+// ![COLOR_SLATE](https://github.com/himanushi/platform-io/assets/27812830/d4475865-22cc-45f1-a2c8-497c48a6b234)
 static constexpr int COLOR_SLATE_400 = 0x953B;
-// ![COLOR_SLATE_500](https://github.com/himanushi/platform-io/assets/27812830/715eb2b0-9779-48c9-9b6e-c439f452afe1)
+// ![COLOR_SLATE](https://github.com/himanushi/platform-io/assets/27812830/715eb2b0-9779-48c9-9b6e-c439f452afe1)
 static constexpr int COLOR_SLATE_500 = 0x63EF;
-// ![COLOR_SLATE_600](https://github.com/himanushi/platform-io/assets/27812830/02cbedd6-9665-426d-893a-c46b7b30e0c4)
+// ![COLOR_SLATE](https://github.com/himanushi/platform-io/assets/27812830/02cbedd6-9665-426d-893a-c46b7b30e0c4)
 static constexpr int COLOR_SLATE_600 = 0x42B5;
-// ![COLOR_SLATE_700](https://github.com/himanushi/platform-io/assets/27812830/fd485ca5-59e5-4e9c-828f-2a8a8550450a)
+// ![COLOR_SLATE](https://github.com/himanushi/platform-io/assets/27812830/fd485ca5-59e5-4e9c-828f-2a8a8550450a)
 static constexpr int COLOR_SLATE_700 = 0x31CE;
-// ![COLOR_SLATE_800](https://github.com/himanushi/platform-io/assets/27812830/54979b69-608d-4218-ad2f-eafa29537bbc)
+// ![COLOR_SLATE](https://github.com/himanushi/platform-io/assets/27812830/54979b69-608d-4218-ad2f-eafa29537bbc)
 static constexpr int COLOR_SLATE_800 = 0x1945;
-// ![COLOR_SLATE_900](https://github.com/himanushi/platform-io/assets/27812830/91914e7a-63ff-47fb-9755-8e4e5b880fad)
+// ![COLOR_SLATE](https://github.com/himanushi/platform-io/assets/27812830/91914e7a-63ff-47fb-9755-8e4e5b880fad)
 static constexpr int COLOR_SLATE_900 = 0x08A9;
-// ![COLOR_SLATE_950](https://github.com/himanushi/platform-io/assets/27812830/129fff2e-c897-4554-b2b8-73af4217b765)
+// ![COLOR_SLATE](https://github.com/himanushi/platform-io/assets/27812830/129fff2e-c897-4554-b2b8-73af4217b765)
 static constexpr int COLOR_SLATE_950 = 0x0022;
 
 // Red
-// ![COLOR_RED_50](https://github.com/himanushi/platform-io/assets/27812830/0562db0b-9b71-41d1-8d46-ff0fef18ac62)
+// ![COLOR_RED](https://github.com/himanushi/platform-io/assets/27812830/0562db0b-9b71-41d1-8d46-ff0fef18ac62)
 static constexpr int COLOR_RED_50 = 0xFFDE;
-// ![COLOR_RED_50](https://github.com/himanushi/platform-io/assets/27812830/c5c893f2-bbcf-46c6-a625-2f5f588c445c)
+// ![COLOR_RED](https://github.com/himanushi/platform-io/assets/27812830/c5c893f2-bbcf-46c6-a625-2f5f588c445c)
 static constexpr int COLOR_RED_100 = 0xFEC6;
-// ![COLOR_RED_50](https://github.com/himanushi/platform-io/assets/27812830/e12adbc3-b2c2-4326-b50d-e3ed7059952c)
+// ![COLOR_RED](https://github.com/himanushi/platform-io/assets/27812830/e12adbc3-b2c2-4326-b50d-e3ed7059952c)
 static constexpr int COLOR_RED_200 = 0xFEA1;
-// ![COLOR_RED_50](https://github.com/himanushi/platform-io/assets/27812830/0b15c285-1a61-4861-b277-b997bee6329a)
+// ![COLOR_RED](https://github.com/himanushi/platform-io/assets/27812830/0b15c285-1a61-4861-b277-b997bee6329a)
 static constexpr int COLOR_RED_300 = 0xFD34;
-// ![COLOR_RED_50](https://github.com/himanushi/platform-io/assets/27812830/a8c07484-c2e1-40be-85ad-ab69acc09222)
+// ![COLOR_RED](https://github.com/himanushi/platform-io/assets/27812830/a8c07484-c2e1-40be-85ad-ab69acc09222)
 static constexpr int COLOR_RED_400 = 0xFB1E;
-// ![COLOR_RED_50](https://github.com/himanushi/platform-io/assets/27812830/c0b382ed-e5e8-4918-9090-93e97ac6e23d)
+// ![COLOR_RED](https://github.com/himanushi/platform-io/assets/27812830/c0b382ed-e5e8-4918-9090-93e97ac6e23d)
 static constexpr int COLOR_RED_500 = 0xE9C4;
-// ![COLOR_RED_50](https://github.com/himanushi/platform-io/assets/27812830/ea67c4f0-03ea-476f-9f62-24bcf2c0e94d)
+// ![COLOR_RED](https://github.com/himanushi/platform-io/assets/27812830/ea67c4f0-03ea-476f-9f62-24bcf2c0e94d)
 static constexpr int COLOR_RED_600 = 0xD904;
-// ![COLOR_RED_50](https://github.com/himanushi/platform-io/assets/27812830/e560ff33-71f3-4d2a-b968-ed280f255a24)
+// ![COLOR_RED](https://github.com/himanushi/platform-io/assets/27812830/e560ff33-71f3-4d2a-b968-ed280f255a24)
 static constexpr int COLOR_RED_700 = 0xB906;
-// ![COLOR_RED_50](https://github.com/himanushi/platform-io/assets/27812830/650f8f55-3d37-4e01-9672-2e9caee118eb)
+// ![COLOR_RED](https://github.com/himanushi/platform-io/assets/27812830/650f8f55-3d37-4e01-9672-2e9caee118eb)
 static constexpr int COLOR_RED_800 = 0x98A5;
-// ![COLOR_RED_50](https://github.com/himanushi/platform-io/assets/27812830/eb32c2a3-61fb-4bc2-a2bd-baa576fe2622)
+// ![COLOR_RED](https://github.com/himanushi/platform-io/assets/27812830/eb32c2a3-61fb-4bc2-a2bd-baa576fe2622)
 static constexpr int COLOR_RED_900 = 0x78E3;
-// ![COLOR_RED_50](https://github.com/himanushi/platform-io/assets/27812830/63de2dec-0585-4b07-8325-d8969237780c)
+// ![COLOR_RED](https://github.com/himanushi/platform-io/assets/27812830/63de2dec-0585-4b07-8325-d8969237780c)
 static constexpr int COLOR_RED_950 = 0x4049;
 
 #endif
