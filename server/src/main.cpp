a6915cb63f7137fb44c9b7441b1d2a587ef0d241
himanushi <himanushi@users.noreply.github.com>
2024-02-26 10:27:26 +0900

    fix

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,6 +1,7 @@
-#include "ESPAsyncWebServer.h"
 #include <ArduinoJson.h>
+#include <ESPAsyncWebServer.h>
 #include <M5Unified.h>
+#include <SPIFFS.h>
 #include <WiFi.h>
 #include <map>
 #include <string>
@@ -10,18 +11,68 @@ AsyncWebServer server(80);
 
 String html = R"html(
 <!doctype html>
-<html lang="ja">
+<html lang="ja" data-theme="dark">
   <head>
     <meta charset="UTF-8" />
     <meta
       name="viewport"
       content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0, minimum-scale=1.0"
     />
-    <title>M5Stack</title>
-    <script src="/van.min.js" type="text/javascript"></script>
-    <script src="/index.js" type="module"></script>
+    <title>Easy Setting</title>
+    <link rel="stylesheet" href="/pico.min.css" />
+    <script type="text/javascript" src="/van.min.js"></script>
+    <script type="module">
+      const { input, button, div, span, pre, h1, h2, label } = van.tags;
+
+      const Settings = async () => {
+        const groups = await fetch("/settings.json").then((res) => res.json());
+        const formData = {};
+
+        const onInput = (setting, e) => {
+          formData[setting.key] = e.target.value;
+        };
+
+        const onSave = () => {
+          fetch("/settings", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+          })
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+            .catch((error) => console.error("Error:", error));
+        };
+
+        return div(
+          ...groups.map((group) => [
+            h2(group.category),
+            ...group.settings.map((setting) => [
+              label(setting.label),
+              input({
+                type: setting.type,
+                name: setting.name,
+                oninput: (e) => onInput(setting, e),
+                autocomplete: "off",
+              }),
+            ]),
+          ]),
+          button({ onclick: onSave }, "Save"),
+        );
+      };
+
+      (async () => {
+        van.add(document.getElementById("main"), [
+          h1("Settings"),
+          await Settings(),
+        ]);
+      })();
+    </script>
   </head>
-  <body></body>
+  <body>
+    <main id="main" class="container"></main>
+  </body>
 </html>
 )html";
 
@@ -34,34 +85,42 @@ const char *vanjs = R"js(
 {let e,t,l,o,r,n,f,s,a,i,d,u,_,w,h,c,g,v,S,y,V,b,m,x,T,j;f=(n=Object).getPrototypeOf,s=document,i={},d=f(a={isConnected:1}),u=f(f),_=(e,t,l,o)=>(e??(setTimeout(l,o),new Set)).add(t),w=(e,t,o)=>{let r=l;l=t;try{return e(o)}catch(e){return console.error(e),o}finally{l=r}},h=e=>e.filter(e=>e.t?.isConnected),c=t=>r=_(r,t,()=>{for(let e of r)e.l=h(e.l),e.o=h(e.o);r=e},1e3),g={get val(){return l?.add(this),this.i},get oldVal(){return l?.add(this),this.u},set val(l){let o=this;if(l!==o.i){o.i=l;let r=[...o.o=h(o.o)];for(let t of r)V(t.f,t.s,t.t),t.t=e;o.l.length?t=_(t,o,T):o.u=l}}},v=e=>({__proto__:g,i:e,u:e,l:[],o:[]}),S=e=>f(e??0)===g,y=(e,t)=>{let l=new Set,r={f:e},n=o;o=[];let f=w(e,l,t);f=(f??s).nodeType?f:new Text(f);for(let e of l)c(e),e.l.push(r);for(let e of o)e.t=f;return o=n,r.t=f},V=(e,t=v(),l)=>{let r=new Set,n={f:e,s:t};n.t=l??o?.push(n)??a,t.val=w(e,r,t.i);for(let e of r)c(e),e.o.push(n);return t},m=t=>new Proxy((l,...o)=>{let[r,...a]=f(o[0]??0)===d?o:[{},...o],_=t?s.createElementNS(t,l):s.createElement(l);for(let[t,o]of n.entries(r)){let r=l=>l?n.getOwnPropertyDescriptor(l,t)??r(f(l)):e,s=l+","+t,a=i[s]??(i[s]=r(f(_))?.set??0),d=t.startsWith("on")?(e,l)=>{let o=t.slice(2);_.removeEventListener(o,l),_.addEventListener(o,e)}:a?a.bind(_):_.setAttribute.bind(_,t),w=f(o??0);w===u&&(!t.startsWith("on")||o.h)&&(o=V(o),w=g),w===g?y(()=>(d(o.val,o.u),_)):d(o)}return b(_,...a)},{get:(t,l)=>t.bind(e,l)}),x=(e,t)=>t?t!==e&&e.replaceWith(t):e.remove(),T=()=>{let l=[...t].filter(e=>e.i!==e.u);t=e;for(let t of new Set(l.flatMap(e=>e.l=h(e.l))))x(t.t,y(t.f,t.t)),t.t=e;for(let e of l)e.u=e.i},j={add:b=(t,...l)=>{for(let o of l.flat(1/0)){let l=f(o??0),r=l===g?y(()=>o.val):l===u?y(o):o;r!=e&&t.append(r)}return t},_:e=>(e.h=1,e),tags:m(),tagsNS:m,state:v,val:e=>S(e)?e.val:e,oldVal:e=>S(e)?e.oldVal:e,derive:V,hydrate:(e,t)=>x(e,y(t,e))},window.van=j;}
 )js";
 
-const char *js = R"js(
-const {button, div, pre} = van.tags
-
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
-
-const Run = ({sleepMs}) => {
-  const steps = van.state(0)
-  ;(async () => { for (; steps.val < 40; ++steps.val) await sleep(sleepMs) })()
-  return pre(() => `${" ".repeat(40 - steps.val)}🚐💨Hello VanJS!${"_".repeat(steps.val)}`)
-}
-
-const Hello = () => {
-  const dom = div()
-  return div(
-    dom,
-    button({onclick: () => van.add(dom, Run({sleepMs: 2000}))}, "Hello 🐌"),
-    button({onclick: () => van.add(dom, Run({sleepMs: 500}))}, "Hello 🐢"),
-    button({onclick: () => van.add(dom, Run({sleepMs: 100}))}, "Hello 🚶‍♂️"),
-    button({onclick: () => van.add(dom, Run({sleepMs: 10}))}, "Hello 🏎️"),
-    button({onclick: () => van.add(dom, Run({sleepMs: 2}))}, "Hello 🚀"),
-  )
-}
-
-van.add(document.body, Hello())
-)js";
-
 const char *settingsJson = R"json(
-{"settings":[{"category": "Wi-Fi","settings":[{"value": null, type: "text", label: "SSID", id: "ssid"},{"value": null, type: "text", label: "Password", id: "ssid"}]}]}
+[
+  {
+    "category": "Wi-Fi",
+    "settings": [
+      { "value": "", "type": "text", "label": "SSID", "key": "wifi_ssid" },
+      {
+        "value": "",
+        "type": "password",
+        "label": "Password",
+        "key": "wifi_password"
+      }
+    ]
+  },
+  {
+    "category": "OpenAI API",
+    "settings": [
+      {
+        "value": "",
+        "type": "password",
+        "label": "API Key",
+        "key": "openai_api_key"
+      },
+      {
+        "value": "gpt-3.5-turbo",
+        "label": "Model",
+        "type": "select",
+        "options": [
+          { "label": "GPT-3.5", "value": "gpt-3.5" },
+          { "label": "GPT-3.5 Turbo", "value": "gpt-3.5-turbo" }
+        ],
+        "key": "openai_model"
+      }
+    ]
+  }
+]
 )json";
 
 std::map<std::string, std::string> settings = {{"ssid", ""}, {"password", ""}};
@@ -91,18 +150,24 @@ void setup() {
   IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(IP);
+  M5.Lcd.println(IP);
+
+  if (!SPIFFS.begin()) {
+    M5.Lcd.println("SPIFFS Mount Failed");
+    return;
+  }
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(200, "text/html", html);
+    request->send(SPIFFS, "/index.html", "text/html");
   });
   server.on("/van.min.js", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(200, "application/javascript", vanjs);
   });
-  server.on("/index.js", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(200, "application/javascript", js);
+  server.on("/pico.min.css", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/pico.min.css", "text/css");
   });
-  server.on("/settings", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(200, "application/json", getSettingsAsJson());
+  server.on("/settings.json", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "application/json", settingsJson);
   });
 
   server.on(
